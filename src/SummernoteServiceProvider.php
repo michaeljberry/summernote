f4361f29bfcd3c656af3bb4ab84584e8b5135268
Michael Berry <berryintlonline@gmail.com>
2018-01-15 14:13:52 -0700

    "First Commit"

---
+++ src/SummernoteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MichaelJBerry\Summernote;
+
+use Illuminate\Support\ServiceProvider;
+
+class SummernoteServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
