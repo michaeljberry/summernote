42e706416d91941b6d88f52e3258903ed52cb8c5
Michael Berry <berryintlonline@gmail.com>
2018-01-16 13:30:42 -0700

    Test loading

---
+++ src/Views/loadSummernote.blade.php
@@ -0,0 +1,9 @@
+
+@push('footscripts')
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.editor').summernote();
+        });
+    </script>
+@endpush
