4573651435d9954730b751a4bf398bb9d5dc742e
Michael Berry <berryintlonline@gmail.com>
2018-01-26 15:23:07 -0700

    Correct font awesome url

--- src/Views/loadSummernote.blade.php
+++ src/Views/loadSummernote.blade.php
@@ -1,9 +1,9 @@
 @push('stylesheets')
-    <link rel="stylesheet" href="{{ asset('summernote/css/summernote.css') }}">
+    <link rel="stylesheet" href="{{ asset("/summernote/css/summernote.css") }}">
 @endpush
 @push('footscripts')
-    <script src="{{ asset('summernote/js/summernote.min.js') }}"></script>
-    <script src="{{ asset('summernote/js/summernote-ext-highlight.js') }}"></script>
+    <script src="{{ asset("/summernote/js/summernote.min.js") }}"></script>
+    <script src="{{ asset("/summernote/js/summernote-ext-highlight.js") }}"></script>
     <script>
         $(document).ready(function() {
             @component('summernote::setupSummernote')
